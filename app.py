85b1e4a42ab0d9c4dfda069c6d55d749705ddb4e
matthewdlang18 <85465968+matthewdlang18@users.noreply.github.com>
2023-08-17 20:36:45 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from game1.game1 import game1
+from game2.game2 import game2
 from main.main import main
 
 app = Flask(__name__)
 
 app.register_blueprint(main)
 app.register_blueprint(game1, url_prefix='/game1')
+app.register_blueprint(game2, url_prefix='/game2')
 
 app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['SESSION_PERMANENT'] = True
