81d28b722b2affd585f0e8b3105b40b8f01a2853
matthewdlang18 <85465968+matthewdlang18@users.noreply.github.com>
2023-08-21 14:05:13 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from game1.game1 import game1
 from game2.game2 import game2
+from game3.game3 import game3
+from game4.game4 import game4
 from main.main import main
 
 app = Flask(__name__)
@@ -8,6 +10,8 @@ app = Flask(__name__)
 app.register_blueprint(main)
 app.register_blueprint(game1, url_prefix='/game1')
 app.register_blueprint(game2, url_prefix='/game2')
+app.register_blueprint(game3, url_prefix='/game3')
+app.register_blueprint(game4, url_prefix='/game4')
 
 app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['SESSION_PERMANENT'] = True
